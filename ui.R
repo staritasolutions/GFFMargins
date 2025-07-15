3e7ad53c3c27540763f88bfb6a1e4f695288198f
22ermiller <22ermiller@gmail.com>
2025-07-15 12:27:39 -0600

    test

--- ui.R
+++ ui.R
@@ -26,7 +26,7 @@ ui <- page_navbar(
     spacer = "0.75rem"
   ),
 
-  title = strong("Get Found First Margins Dashboard"),
+  title = strong("Get Found First Margins Dashboard 2"),
   underline = FALSE,
   tags$head(
     tags$style(HTML(
